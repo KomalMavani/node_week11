d0993ca2e49d1779c1ab7dedf201dd2626134f8f
KomalMavani <102403515+KomalMavani@users.noreply.github.com>
2024-11-23 11:10:23 -0500

    get/post product API

--- index.js
+++ index.js
@@ -10,6 +10,29 @@ app.get('/test', (req, res) => {
     res.send("Hello World test");
 })
 
+let jsonData = [];
+
+app.post('/addProducts', (req, res) => {
+  let bodyData = req.body;
+  jsonData.push(bodyData);
+
+  return res.status(200).send({
+    status: 200,
+    message: "Product added successfully !!",
+    data: bodyData,
+    error: false,
+  });
+})
+
+app.get('/getProducts', (req, res) => {  
+    return res.status(200).send({
+      status: 200,
+      message: "Get product list successfully !!",
+      data: jsonData,
+      error: false,
+    });
+})
+
 const PORT = 8080;
 
 app.listen(PORT, () => {
